7412bf22c594c39751401c2f91a4c59516685c8a
Kemunto Maragia <kmaragia@email.wm.edu>
2020-09-11 01:33:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,25 +1,29 @@
-def instantiate_new_array()
-  new_array = []
-  return new_array
+def instantiate_new_array
+  return []
 end
-def array_with_two_elements()
-  new_array = [1,2]
+
+def array_with_two_elements
+  return [1,2]
 end
-def first_element(array)
-  return array[0]
+
+def first_element(arr)
+  return arr[0]
 end
-def third_element(array)
-  return array[2]
+
+def third_element(arr)
+  return arr[2]
 end
-def last_element(array)
-  return array[-1]
+
+def last_element(arr)
+  return arr[-1]
 end
-def first_element_with_array_methods(array)
-  return array.first
-end  
-def last_element_with_array_methods(array)
-  return array.last
-end  
-def length_of_array(array)
-  return array.length
+
+def first_element_with_array_methods(arr)
+  return arr.shift
 end
+def last_element_with_array_methods(arr)
+  return arr.pop
+end
+def length_of_array(arr)
+  return arr.length
+end 
